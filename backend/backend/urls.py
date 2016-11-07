e06895f0f8e8227e52900c1e3703e9d4426b0740
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2016-11-07 21:47:11 +0100

    Use a list to define routed urls
    https://docs.djangoproject.com/en/1.8/ref/urls/ warns that
    django.conf.urls.pattern is deprecated since Django 1.8, and it has been
    removed in 1.10.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'platal.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -9,4 +9,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('backend.api.urls', namespace='api')),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
-)
+]
